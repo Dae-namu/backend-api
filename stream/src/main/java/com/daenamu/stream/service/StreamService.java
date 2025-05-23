d4c0ef090d40d3aa49f4d4bac2d32597e0654ed9
HyominAn0401 <slhm7547@naver.com>
2025-05-23 12:21:20 +0900

    fix: presignedUrl null issue

--- stream/src/main/java/com/daenamu/stream/service/StreamService.java
+++ stream/src/main/java/com/daenamu/stream/service/StreamService.java
@@ -3,7 +3,10 @@ package com.daenamu.stream.service;
 import com.daenamu.stream.dto.StreamResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
@@ -15,10 +18,22 @@ import java.time.Duration;
 public class StreamService {
 
     private final S3Presigner s3Presigner;
+    private final S3Client s3Client;
 
     public StreamResponseDto getStreamByEpisodeId(Long dramaId, Long episodeId){
         String bucketName = "daenamu-bucket";
         String objectKey = dramaId + "/episode-"+episodeId+".mp4";
+
+        try{
+            s3Client.headObject(HeadObjectRequest.builder()
+                    .bucket(bucketName).key(objectKey).build());
+        }
+        catch (S3Exception e){
+            if(e.statusCode()==404){
+                return new StreamResponseDto(episodeId, null);
+            }
+            throw e;
+        }
 
         GetObjectRequest getObjectRequest = GetObjectRequest.builder()
                 .bucket(bucketName)
